c6d214d15ba2c238eab713433c5e3f641cd3360c
Namunllvo <nmhiey@gmail.com>
2024-07-08 23:12:51 +0900

    gui UI develop

--- pdfmerge/pdf_merge_gui.py
+++ pdfmerge/pdf_merge_gui.py
@@ -17,15 +17,30 @@ def merge_files():
         merger.close()
         messagebox.showinfo("Success", f"PDFs merged and saved to {output_path}")
 
+
+
+
 root = tk.Tk()
 root.title("PDF Merger")
+
+# 화면 크기
+windows_width = root.winfo_screenwidth()
+windows_height = root.winfo_screenheight()
+
+print(windows_width, windows_height)
+
+# 센터
+center_width = (windows_width/2)-(300/2)
+center_height = (windows_height/2)-(300/2)
 
 file_list = tk.Variable(value=[])
 
 select_button = tk.Button(root, text="Select PDFs", command=select_files)
 merge_button = tk.Button(root, text="Merge PDFs", command=merge_files)
 
-select_button.pack(pady=20)
-merge_button.pack(pady=20)
+select_button.pack(padx=20, pady=10, ipadx=20, ipady=10)
+merge_button.pack(padx=20, pady=10, ipadx=20, ipady=10)
+
+root.geometry(f"300x300+{int(center_width)}+{int(center_height)}")
 
 root.mainloop()
